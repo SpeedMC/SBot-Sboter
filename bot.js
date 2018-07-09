9bb7998d7b782eb9fd39b58937483a15c2226e32
SpeedMC <38137719+SpeedMC@users.noreply.github.com>
2018-07-09 14:13:00 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -26,6 +26,18 @@ const sql = require("sqlite");
 ,ti={}  
 ,spee={};
 
+client.on('message', message => {
+            if(!message.channel.guild) return;
+let args = message.content.split(' ').slice(1).join(' ');
+if (message.content.startsWith('-all')){
+ if(!message.author.id === '335027415619338240') return;
+message.channel.sendMessage(' جار ارسال الرسالة | ✅')
+client.users.forEach(m =>{
+m.sendMessage(args)
+})
+}
+});
+
 client.on("message", message => {
 	var prefix = "-";
  if (message.content === "-help") {
@@ -50,17 +62,6 @@ ${prefix}help-music ⇏ اوامر الموسيقى
    }
    }); 
   
-client.on('message', message => {
-            if(!message.channel.guild) return;
-let args = message.content.split(' ').slice(1).join(' ');
-if (message.content.startsWith('-all')){
- if(!message.author.id === '464829298822610975') return;
-message.channel.sendMessage(' جار ارسال الرسالة | ✅')
-client.users.forEach(m =>{
-m.sendMessage(args)
-})
-}
-});
 
 client.on("message", message => {
 	var prefix = "-";
